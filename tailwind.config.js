875a8adccb7d0ac2fb97df780a54b67133074e01
Jake <55953689+jakecreely@users.noreply.github.com>
2023-02-10 12:21:04 +1100

    Customising tailwind to fit design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {      
+      fontFamily: {
+      'jost': ['Jost', 'sans-serif']
+      },
+    colors: {
+      primary: "#AD1FEA",
+      secondary: "#4661E6",
+      tertiary: "#4461E6",
+      warning: "#D73737",
+      black: "#3A4374"
+    }
+  },
   },
   plugins: [],
 }
