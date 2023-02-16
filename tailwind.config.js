621032927812f107abe1df9cd57bee3e0567e89f
Jake <55953689+jakecreely@users.noreply.github.com>
2023-02-16 16:04:46 +1100

    Adding background images and additional colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,15 @@ module.exports = {
       secondary: "#4661E6",
       tertiary: "#4461E6",
       warning: "#D73737",
-      black: "#3A4374"
+      black: "#3A4374",
+      blue: "#373F68",
+      lightBlue: '#62BCFA',
+      lightGrey: '#647196'
+    },
+    backgroundImage: {
+      'mobile': "url('/assets/suggestions/mobile/background-header.png')",
+      'tablet': "url('/assets/suggestions/tablet/background-header.png')",
+      'desktop': "url('/assets/suggestions/desktop/background-header.png')",
     }
   },
   },
