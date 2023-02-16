69ccf54b0d434b840ac0530c89f7e6a76456e42e
Jake <55953689+jakecreely@users.noreply.github.com>
2023-02-16 16:04:32 +1100

    Suggestion by ID Template

--- pages/feedback/detail/[id].tsx
+++ pages/feedback/detail/[id].tsx
@@ -1,12 +1,39 @@
 import { useRouter } from 'next/router'
+import Card from '@/components/suggestionCard'
 
 export default function Details() {
     const router = useRouter()
     const { id } = router.query
 
+    let suggestionById: {
+        title: string, 
+        description: string,
+        tags: string[],
+        upvotes: number,
+        comments: number,
+      } = {
+        title: "Add dark mode",
+        description: "It would be great if you could add a dark mode to the website",
+        tags: ["Feature"],
+        upvotes: 10,
+        comments: 2,
+      }
+
     return (
-        <div>
-            <h1>Details {id}</h1>
+        <div className='grid grid-cols-1 p-6'>
+            <div className='pb-6'>
+                <button className='bg-transparent px-2 py-2' onClick={() => router.back()}>Go Back</button>
+                <button className='px-2 py-2'>Edit Feedback</button>
+            </div>
+            <div className=''>
+                <Card 
+                title={suggestionById.title} 
+                description={suggestionById.description}
+                tags={suggestionById.tags}
+                upvotes={suggestionById.upvotes}
+                comments={suggestionById.comments}
+                />
+            </div>
         </div>
     )
 }
